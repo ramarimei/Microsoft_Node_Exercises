da6e28f3d469a0d3cdd05978ef2c2bc76dd579b8
Ramari Heperi <ramarimei@Ramaris-MacBook-Air.local>
2021-02-05 09:39:41 +1300

    commit

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -1,41 +1,37 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 let products = [];
 
+app.route('/products')
+ .get((req, res) => {
+   res.json(products);
+ })
+ .post((req, res) => {
+   const newProduct = { ...req.body, id: products.length + 1 }
+   products = [...products, newProduct]
+   res.json(newProduct);
+ })
+.put((req, res) => {
+   let updatedProduct;
+   products = products.map(p => {
+     if (p.id === req.body.id) {
+       updatedProduct = { ...p, ...req.body };
+       return updatedProduct;
+     }
+     return p;
+   })
+   res.json(updatedProduct);
+ })
+ .delete((req, res) => {
+   const deletedProduct = products.find(p => p.id === +req.body.id);
+   products = products.filter(p => p.id !== +req.body.id);
+   res.json(deletedProduct);
+ })
 
-app.post('/products', function(req, res) {
-  const newProduct = { ...req.body, id: products.length + 1 }
-  products = [ ...products, newProduct]
-  res.json(newProduct);
-});
-
-
-app.put('/products', function(req, res) {
-  let updatedProduct;
-  products = products.map(p => {
-    if (p.id === req.body.id) {
-      updatedProduct = { ...p, ...req.body };
-      return updatedProduct;
-    }
-    return p;
-  })
-  res.json(updatedProduct);
-});
-
-app.delete('/products/:id', function(req, res) {
-  const deletedProduct = products.find(p => p.id === +req.params.id);
-  products = products.filter(p => p.id !== +req.params.id);
-  res.json(deletedProduct);
-});
-
-app.get('/products', (req, res) => {
-  res.json(products);
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
   
